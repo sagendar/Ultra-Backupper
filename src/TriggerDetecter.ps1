b66c0a53985d4e9145becaf2ce3999a9c0854ec3
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-22 16:25:18 +0200

    changed to start-process

--- src/TriggerDetecter.ps1
+++ src/TriggerDetecter.ps1
@@ -1,32 +1,31 @@
 ﻿Register-WmiEvent -Class win32_VolumeChangeEvent -SourceIdentifier volumeChange
 write-host (get-date -format s) " Beginning script..."
-do{
-$newEvent = Wait-Event -SourceIdentifier volumeChange
-$eventType = $newEvent.SourceEventArgs.NewEvent.EventType
-$eventTypeName = switch($eventType)
-{
-1 {"Configuration changed"}
-2 {"Device arrival"}
-3 {"Device removal"}
-4 {"docking"}
+while (1 -eq 1) {
+    $newEvent = Wait-Event -SourceIdentifier volumeChange
+    $eventType = $newEvent.SourceEventArgs.NewEvent.EventType
+    $eventTypeName = switch ($eventType) {
+        1 {"Configuration changed"}
+        2 {"Device arrival"}
+        3 {"Device removal"}
+        4 {"docking"}
+    }
+    write-host (get-date -format s) " Event detected = " $eventTypeName
+    if ($eventType -eq 2) {
+        $driveLetter = $newEvent.SourceEventArgs.NewEvent.DriveName
+        $driveLabel = ([wmi]"Win32_LogicalDisk='$driveLetter'").VolumeName
+        write-host (get-date -format s) " Drive name = " $driveLetter
+        write-host (get-date -format s) " Drive label = " $driveLabel
+        write-host (get-date -format s) " Starting task in 3 seconds..."
+        start-sleep -seconds 3
+
+        $path = $MyInvocation.MyCommand.Path
+        $path = $path -replace "TriggerDetecter.ps1", ""
+
+        $path = $path + "USB_Backup_Script.ps1"
+        $arguments = "-file " + $path
+        Start-Process powershell.exe -ArgumentList $arguments
+    }
+    
+    Remove-Event -SourceIdentifier volumeChange
 }
-write-host (get-date -format s) " Event detected = " $eventTypeName
-if ($eventType -eq 2)
-{
-$driveLetter = $newEvent.SourceEventArgs.NewEvent.DriveName
-$driveLabel = ([wmi]"Win32_LogicalDisk='$driveLetter'").VolumeName
-write-host (get-date -format s) " Drive name = " $driveLetter
-write-host (get-date -format s) " Drive label = " $driveLabel
-write-host (get-date -format s) " Starting task in 3 seconds..."
-start-sleep -seconds 3
-
-$path = $MyInvocation.MyCommand.Path
-$path = $path -replace "TriggerDetecter.ps1", ""
-
-$path = $path + "USB_Backup_Script.ps1"
-
-& $path 
-}
-Remove-Event -SourceIdentifier volumeChange
-} while (1-eq1) #Loop until next event
 Unregister-Event -SourceIdentifier volumeChange
