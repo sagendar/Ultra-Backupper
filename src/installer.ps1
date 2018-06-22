e0739092089583e1e0f675aa882d1a5ae1250ac1
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-22 14:21:30 +0200

    configured task details

--- src/installer.ps1
+++ src/installer.ps1
@@ -5,4 +5,4 @@ $path = $path + 'TriggerDetecter.ps1'
 $yeet = '-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden ' + $path
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $yeet
 $trigger =  New-ScheduledTaskTrigger -AtStartup
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "USBBackup" -Description "Automatic USB Backupper" 
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "UltraBackupper" -Description "Automatic Ultra USB Backupper" 
