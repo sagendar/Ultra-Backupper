311a433917b870cd55b3bde7581265b9fb99958d
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-22 14:44:26 +0200

    refactoring

--- src/TriggerDetecter.ps1
+++ src/TriggerDetecter.ps1
@@ -1,5 +1,4 @@
-﻿#Requires -version 2.0
-Register-WmiEvent -Class win32_VolumeChangeEvent -SourceIdentifier volumeChange
+﻿Register-WmiEvent -Class win32_VolumeChangeEvent -SourceIdentifier volumeChange
 write-host (get-date -format s) " Beginning script..."
 do{
 $newEvent = Wait-Event -SourceIdentifier volumeChange
@@ -18,11 +17,15 @@ $driveLetter = $newEvent.SourceEventArgs.NewEvent.DriveName
 $driveLabel = ([wmi]"Win32_LogicalDisk='$driveLetter'").VolumeName
 write-host (get-date -format s) " Drive name = " $driveLetter
 write-host (get-date -format s) " Drive label = " $driveLabel
-# Execute process if drive matches specified condition(s)
 write-host (get-date -format s) " Starting task in 3 seconds..."
 start-sleep -seconds 3
-start-process ".\USB_Backup_Script.ps1"
-& ".\USB_Backup_Script.ps1"
+
+$path = $MyInvocation.MyCommand.Path
+$path = $path -replace "TriggerDetecter.ps1", ""
+
+$path = $path + "USB_Backup_Script.ps1"
+
+& $path 
 }
 Remove-Event -SourceIdentifier volumeChange
 } while (1-eq1) #Loop until next event
--- src/USB_Backup_Script.ps1
+++ src/USB_Backup_Script.ps1
@@ -1,28 +1,32 @@
-$d = get-date
-echo "USB Backup started" > c:\temp\USBBackup.log
+$date = Get-Date -Format yyyyMMddTHHmm
+$path = $MyInvocation.MyCommand.Path
+$path = $path -replace "USB_Backup_Script.ps1", ""
+$backup = $path + 'backup'
+$log = $path + "USBBackup.log"
+
+echo "UltraBackupper started" > $log
 
 try
 {
-    $x = gwmi win32_diskdrive | ?{$_.interfacetype -eq "USB"} | %{gwmi -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  %{gwmi -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | %{$_.deviceid}
+    $dir = gwmi win32_diskdrive | ?{$_.interfacetype -eq "USB"} | %{gwmi -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  %{gwmi -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | %{$_.deviceid}
 }
 catch
 {
-    echo "Caught Error: Unable to locate External USB Drive for Backup." >> c:\temp\USBBackup.log
+    echo "Caught Error: Unable to locate External USB Drive for Backup." >> $log
     exit 
 }
-
-if ($x) {}
+if ($dir) {}
 else
 {
-    echo "Null Drive Letter: Unable to locate External USB Drive [USB_D2D] for D2D Backup." >> c:\temp\USBBackup.log
+    echo "Null Drive Letter: Unable to locate External USB Drive [USB_D2D] for D2D Backup." >> $log
     exit 
 }    
-
+    
 #COMPRESS USB STICK CONTENT
 Compress-Archive -Path $x -CompressionLevel Optimal -DestinationPath C:\temp\$d
 
 #UPLOAD COMPRESSED ARCHIVE TO GOOGLE DRIVE
 
 
-echo $d >> c:\temp\USBBackup.log
-echo Done. >> c:\temp\USBBackup.log
+echo $date >> .\USBBackup.log
+echo Done. >> .\USBBackup.log
