ae9cbd4f79da39b7255b3950897b01f7b4096af5
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-22 14:18:59 +0200

    added installer

---
+++ src/installer.ps1
@@ -0,0 +1,8 @@
+﻿ipmo ScheduledTasks 
+$path = $MyInvocation.MyCommand.Path
+$path = $path -replace "installer.ps1", ""
+$path = $path + 'TriggerDetecter.ps1' 
+$yeet = '-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden ' + $path
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $yeet
+$trigger =  New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "USBBackup" -Description "Automatic USB Backupper" 
