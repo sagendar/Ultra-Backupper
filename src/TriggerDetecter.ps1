cb4ecda75d92eeb902201ee875e62e19a8520db6
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-22 14:04:22 +0200

    add triggerdetector

---
+++ src/TriggerDetecter.ps1
@@ -0,0 +1,29 @@
+﻿#Requires -version 2.0
+Register-WmiEvent -Class win32_VolumeChangeEvent -SourceIdentifier volumeChange
+write-host (get-date -format s) " Beginning script..."
+do{
+$newEvent = Wait-Event -SourceIdentifier volumeChange
+$eventType = $newEvent.SourceEventArgs.NewEvent.EventType
+$eventTypeName = switch($eventType)
+{
+1 {"Configuration changed"}
+2 {"Device arrival"}
+3 {"Device removal"}
+4 {"docking"}
+}
+write-host (get-date -format s) " Event detected = " $eventTypeName
+if ($eventType -eq 2)
+{
+$driveLetter = $newEvent.SourceEventArgs.NewEvent.DriveName
+$driveLabel = ([wmi]"Win32_LogicalDisk='$driveLetter'").VolumeName
+write-host (get-date -format s) " Drive name = " $driveLetter
+write-host (get-date -format s) " Drive label = " $driveLabel
+# Execute process if drive matches specified condition(s)
+write-host (get-date -format s) " Starting task in 3 seconds..."
+start-sleep -seconds 3
+start-process "SCRIPT PATH"
+& "SCRIPT PATH"
+}
+Remove-Event -SourceIdentifier volumeChange
+} while (1-eq1) #Loop until next event
+Unregister-Event -SourceIdentifier volumeChange
