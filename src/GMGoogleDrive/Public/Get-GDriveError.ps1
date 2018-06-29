5c1b084e7e33ebce27708ae2f4757a6118b8862a
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-29 13:30:27 +0200

    added googledrive library

---
+++ src/GMGoogleDrive/Public/Get-GDriveError.ps1
@@ -0,0 +1,80 @@
+﻿<#
+.SYNOPSIS
+    Get GoogleDrive Error responce
+.DESCRIPTION
+    Get GoogleDrive Error responce
+.PARAMETER Error
+    Error record to decode
+.PARAMETER Exception
+    Exception object to decode
+.OUTPUTS
+    Json with error item as PSObject
+.EXAMPLE
+    try { Get-GDriveItemProperty -AccessToken 'error token' -id 'error id' } catch { $err = $_ }
+    Get-GDriveError $err
+.NOTES
+    Author: Max Kozlov
+.LINK
+#>
+function Get-GDriveError {
+[CmdletBinding(DefaultParameterSetName='ex')]
+param(
+    [Parameter(Mandatory, Position=0, ParameterSetName='er')]
+    [System.Management.Automation.ErrorRecord]$Error,
+
+    [Parameter(Mandatory, Position=0, ParameterSetName='ex')]
+    [Exception]$Exception
+)
+    $result = [PSCustomObject]@{
+        Type = $null
+        StatusCode = $null
+        Message = ''
+        Error = ''
+    }
+    if ($Error) {
+        $Exception = $Error.Exception
+    }
+    if ($Exception) {
+        $result.Type = $Exception.GetType()
+        $result.Message = $Exception.Message
+        if ($Exception -is [System.Net.WebException]) {
+            $response = $Exception.Response
+            $result.StatusCode = $response.StatusCode
+            $result.Message = $response.StatusDescription
+            try {
+                $Encoding = [Text.Encoding]::GetEncoding($response.CharacterSet)
+            }
+            catch {
+                $Encoding = [Text.Encoding]::UTF8
+            }
+            try {
+                Write-Verbose "StatusCode: $($response.StatusCode)"
+                Write-Verbose "ContentLength: $($response.ContentLength)"
+                Write-Verbose "ContentType: $($response.ContentType)"
+                Write-Verbose "CharacterSet: $($response.CharacterSet)"
+                $stream = $response.GetResponseStream()
+
+                try {
+                    $s = $Encoding.GetString($stream.ToArray())
+                    try {
+                        $result.Error = $s | ConvertFrom-Json | Select-Object -ExpandProperty error
+                    }
+                    catch {
+                        Write-Warning "Can't decode error"
+                    }
+                }
+                finally {
+        # ?
+        #            $stream.Close()
+        #            $stream.Dispose()
+                }
+            }
+            finally {
+        # ?
+        #        $response.Close()
+        #        $response.Dispose()
+            }
+        }
+    }
+    $result
+}
---
+++ src/GMGoogleDrive/Public/Remove-GDriveItem.ps1
@@ -0,0 +1,58 @@
+﻿<#
+.SYNOPSIS
+    Move GoogleDrive Item to trash or remove it permanently
+.DESCRIPTION
+    Move GoogleDrive Item to trash or remove it permanently
+.PARAMETER ID
+    File ID to remove
+.PARAMETER Permanently
+    Permanently remove item. If not set, item moved to trash
+.PARAMETER AccessToken
+    Access Token for request
+.EXAMPLE
+    # Trash item
+    Remove-GDriveItem -AccessToken $access_token -ID '0BAjkl4cBDNVpVbB5nGhKQ195aU0'
+.EXAMPLE
+    # Remove item
+    Remove-GDriveItem -AccessToken $access_token -ID '0BAjkl4cBDNVpVbB5nGhKQ195aU0' -Permanently
+.OUTPUTS
+    Json with item metadata as PSObject
+    or None if removed permanently
+.NOTES
+    Author: Max Kozlov
+.LINK
+    Rename-GDriveItem
+    Move-GDriveItem
+    Set-GDriveItemProperty
+#>
+function Remove-GDriveItem {
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='High')]
+param(
+    [Parameter(Mandatory, Position=0)]
+    [string]$ID,
+
+    [switch]$Permanently,
+
+    [Parameter(Mandatory)]
+    [string]$AccessToken
+)
+#https://developers.google.com/drive/v3/reference/files/delete
+
+    if ($Permanently) {
+        $Headers = @{
+            "Authorization" = "Bearer $AccessToken"
+            "Content-type"  = "application/json"
+        }
+        $Uri = $GDriveUri + $ID
+        Write-Verbose "URI: $Uri"
+
+        if ($PSCmdlet.ShouldProcess("Remove Item $ID")) {
+            Invoke-RestMethod -Uri $Uri -Method Delete -Headers $Headers @GDriveProxySettings
+        }
+    }
+    else {
+        if ($PSCmdlet.ShouldProcess("Move Item $ID to trash")) {
+            Set-GDriveItemProperty @PSBoundParameters -JsonProperty '{ "trashed":"true" }'
+        }
+    }
+}
